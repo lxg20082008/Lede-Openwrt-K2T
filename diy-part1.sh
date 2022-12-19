f127c5356aeb2206674de899e23172be5284a2d9
lxg20082008 <61457095+lxg20082008@users.noreply.github.com>
2022-12-20 00:39:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # 添加源
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # 应用过滤OpenAppFilter 【luci oaf app】
 # 和加速模块（Turbo ACC)有冲突，需要关闭Turboo ACC后使用
